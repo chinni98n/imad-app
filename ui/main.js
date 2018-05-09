68ca26c1b7cb22c8089a3e7c9e2f83781aff67b4
chinni98n <39025720+chinni98n@users.noreply.github.com>
2018-05-09 15:34:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,20 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function() {
     //Make a request to the countr endpoint
+    var request = new XMLHttpRequest();
     //Capture the response and store it in a variable
-    //Render the variable in the correct span
-    counter=counter+1;
-    var span = document.getElementById('count');
+    request.onreadystateChange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status==200){
+                var counter=request.responseText;
+                var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    };
+    //Make the request
+    request.open('GET','https://chinni98n.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
