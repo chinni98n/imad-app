fb9c361a6be21ceba85a1230aa13186dc4754b9d
chinni98n <39025720+chinni98n@users.noreply.github.com>
2018-05-09 14:24:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,6 @@ function moveRight() {
     img.style.marginLeft = marginLeft + px;
 }
 img.onclick = function() {
-    var interval=setInterval(moveLeft,100);
+    var interval=setInterval(moveRight,100);
     //img.style.marginLeft = '100px';
 };
