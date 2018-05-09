ada823b82e94125c39b92e23ece60b001286a351
chinni98n <39025720+chinni98n@users.noreply.github.com>
2018-05-09 17:54:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick=function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick = function() {
     //make a request to the server and send the name
@@ -51,6 +50,9 @@ submit.onclick = function() {
             }
                             //Not done yet
     };
+    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     //Make the request
     request.open('GET','http://chinni98n.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
