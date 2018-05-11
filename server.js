637e5e21812dd0679d12cd1b8447d50391a65b47
chinni98n <39025720+chinni98n@users.noreply.github.com>
2018-05-11 18:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.get('/articles/:articleName',function(req,res){
            }
            else{
                var articleData = result.rows[0];
-               res.send(createTemplate(articleData]));
+               res.send(createTemplate(articleData));
            }
        }
     });
