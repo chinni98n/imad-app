cf491cf3e81141332b6b8e958c02cea5b0d986a9
chinni98n <39025720+chinni98n@users.noreply.github.com>
2018-05-11 18:24:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/articles/:articleName',function(req,res){
            res.status(500).send(err.toString());
        } 
        else {
-           if(res.rows.length===0) {
+           if(result.rows.length===0) {
                res.Status(404).send('Article not found');
            }
            else{
